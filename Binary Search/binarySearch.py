6bc30d826a2d2e6be3ea93ca98475710c3dd22b6
Crawford Patten <89034309+CrawfordPat@users.noreply.github.com>
2022-07-04 18:57:29 -0700

    Fixed it
    I did a stupid

--- Binary Search/binarySearch.py
+++ Binary Search/binarySearch.py
@@ -2,20 +2,15 @@ import random
 
 def binarySearch(searchArray, target):
     bottomLimit = 0
-    topLimit = len(searchArray) + 1
+    topLimit = len(nums)-1
 
-    mid = bottomLimit + (topLimit - bottomLimit) // 2
+    while bottomLimit <= topLimit:
+        mid = bottomLimit + (topLimit - bottomLimit) // 2
 
-    while mid != target:
-        if mid > target:
-            topLimit = mid
-            mid = bottomLimit + (topLimit - bottomLimit) // 2
-        
-        elif mid < target:
-            bottomLimit = mid
-            mid = bottomLimit + (topLimit - bottomLimit) // 2
-
+        if nums[mid] < target:
+            bottomLimit = mid + 1
+        elif nums[mid] > target:
+            topLimit = mid - 1
         else:
-            return -1
-            
-    return mid
+            return mid
+    return -1
