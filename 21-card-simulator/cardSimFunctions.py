490b3632486423c3211374407d133debc0ce6000
Crawfordpatt <crawfordpatten2@gmail.com>
2022-07-27 19:46:19 -0700

    IT WORKS

--- 21-card-simulator/cardSimFunctions.py
+++ 21-card-simulator/cardSimFunctions.py
@@ -1,10 +1,10 @@
 import random
 
-# Create an array of all cards in the deck
+# Array of all cards in the deck
 cardDeck = ["AS", "KS", "QS", "JS", "TS", "9S", "8S", "7S", "6S", "5S", "4S", "3S", "2S", "AH", "KH", "QH", "JH", "TH", "9H", "8H", "7H", "6H", "5H", "4H", "3H", "2H", "AC", "KC", "QC", "JC", "TC", "9C", "8C", "7C", "6C", "5C", "4C", "3C", "2C", "AD", "KD", "QD", "JD", "TD", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D"]
 
 # Pick 21 random, non duplicate cards from the deck
-def pickCards(deck, num):
+def pickCards(num):
     cards = []
     while len(cards) < num:
         card = cardDeck[random.randint(0, len(cardDeck)-1)]
@@ -12,8 +12,6 @@ def pickCards(deck, num):
             cards.append(card)
     return cards
 
-twentyOneDeck = pickCards(cardDeck, 21)
-print(twentyOneDeck)
 
 def makeColumns(deck):
     col1 = []
@@ -30,8 +28,15 @@ def makeColumns(deck):
 
     return col1, col2, col3
 
-# Make and print the columns
-col1, col2, col3 = makeColumns(twentyOneDeck)
-print(col1)
-print(col2)
-print(col3)
+def printColumns(deck):
+    col1, col2, col3 = makeColumns(deck)
+    for i in range(len(col1)):
+        print(col1[i], col2[i], col3[i])
+
+
+def stack(bottom, middle, top):
+    stack = []
+    stack.extend(top)
+    stack.extend(middle)
+    stack.extend(bottom)
+    return stack
---
+++ 21-card-simulator/main.py
@@ -0,0 +1,38 @@
+import cardSimFunctions as csf
+
+def main():
+    numOfCards = int(input("How many cards do you want in the deck: "))
+    deck = csf.pickCards(numOfCards)
+
+    numOfLoops = int(input("How many times do you want to stack the columns: "))
+
+    for i in range(numOfLoops):
+        deck = cardLoop(deck)
+        print("\n")
+
+    userCard = str(input("Is your card the " + str(deck[10]) + " (y/n): "))
+
+    if userCard == "y":
+        print("Great!")
+    else:
+        print("Shoot!")
+
+
+def cardLoop(deck):
+    col1, col2, col3 = csf.makeColumns(deck)
+    csf.printColumns(deck)
+
+    whichColumn = int(input("Which column is your card in: "))
+    if whichColumn == 1:
+        stack = csf.stack(col2, col1, col3)
+    elif whichColumn == 2:
+        stack = csf.stack(col3, col2, col1)
+    elif whichColumn == 3:
+        stack = csf.stack(col1, col3, col2)
+    else:
+        print("Invalid input")
+        return
+    
+    return stack
+
+main()
